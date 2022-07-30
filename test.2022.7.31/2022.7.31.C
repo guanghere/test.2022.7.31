9619fb9c0c8a3f10080ff4d8fb3e3c76c9d1c8d4
HZKD513 <544228362@qq.com>
2022-07-30 14:20:12 +0800

    添加项目文件。

---
+++ test.2022.7.31/2022.7.31.C
@@ -0,0 +1,40 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include<stdio.h>
+
+//枚举常量
+//enum dog
+//{
+//	RED=5,
+//	GREEN,
+//	BULE
+//};
+//
+//int main()
+//{
+//	printf("%d\n", RED);
+//	printf("%d\n", GREEN);
+//	printf("%d\n", BULE);
+//	return 0;
+//}
+
+
+//int main()
+//{
+//	char arr1[] = "abcd";
+//	char arr2[] = { 'a','b','c','d'};
+//
+//	printf("%s\n",arr1);
+//	printf("%s\n",arr2);
+//
+//	return 0;
+//}
+
+//
+//int main()
+//{
+//        printf("%d\n", strlen("abcdef"));
+//
+//        printf("%d\n", strlen("c:\test\328\test.c"));
+//                                    // \32被解析成一个转义字符
+//	return 0;
+//}
